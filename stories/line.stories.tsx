600612f2445ce26fdd943ce2ce14e809e41eaad4
Tmk <i@tmk.im>
2025-08-15 23:21:46 +0800

    feat: add line-function example

--- stories/line.stories.tsx
+++ stories/line.stories.tsx
@@ -1051,3 +1051,33 @@ export function DynamicData2() {
     </LineChart>
   );
 }
+
+export function LineFunction() {
+  function func(x: number) {
+    x /= 10;
+    return Math.sin(x) * Math.cos(x * 2 + 1) * Math.sin(x * 3 + 2) * 50;
+  }
+  function generateData() {
+    let data = [];
+    for (let i = -200; i <= 200; i += 0.1) data.push([i, func(i)]);
+    return data;
+  }
+
+  return (
+    <LineChart
+      style={{ width: 480, height: 300 }}
+      animation={false}
+      xAxis={{ name: 'x', minorTick: { show: true }, minorSplitLine: { show: true } }}
+      yAxis={{ name: 'y', min: -100, max: 100, minorTick: { show: true }, minorSplitLine: { show: true } }}
+      series={[{ type: 'line', showSymbol: false, clip: true, data: generateData() }]}
+      grid={{ top: 40, left: 50, right: 40, bottom: 50 }}
+    >
+      <DataZoom
+        dataZoom={[
+          { show: true, type: 'inside', filterMode: 'none', xAxisIndex: [0], startValue: -20, endValue: 20 },
+          { show: true, type: 'inside', filterMode: 'none', yAxisIndex: [0], startValue: -20, endValue: 20 },
+        ]}
+      />
+    </LineChart>
+  );
+}
