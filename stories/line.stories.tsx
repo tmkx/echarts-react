b7e4b716c6d2af99874feec39580376ba4878ff0
Tmk <i@tmk.im>
2025-08-16 16:42:07 +0800

    feat: add line-draggable example

--- stories/line.stories.tsx
+++ stories/line.stories.tsx
@@ -1996,3 +1996,96 @@ export function LineTooltipTouch() {
     </LineChart>
   );
 }
+
+// FIXME: The tooltip behaves inconsistently when dragging the points.
+export function LineDraggable() {
+  type GraphicComponentLooseOption = Exclude<React.ComponentProps<typeof Graphic>['graphic'], any[] | undefined>;
+  const symbolSize = 20;
+  const [data, setData] = React.useState(() => [
+    [40, -10],
+    [-30, -5],
+    [-76.5, 20],
+    [-63.5, 40],
+    [-22.1, 50],
+  ]);
+  const [graphic, setGraphic] = React.useState<GraphicComponentLooseOption[]>([]);
+  const chartRef = React.useRef<echarts.ECharts>(null);
+  const dataRef = React.useRef(data);
+  dataRef.current = data;
+
+  React.useEffect(() => {
+    const myChart = chartRef.current;
+    if (!myChart) return;
+    const showTip = (dataIndex: number) => {
+      myChart.dispatchAction({ type: 'showTip', seriesIndex: 0, dataIndex });
+    };
+    const hideTip = () => {
+      myChart.dispatchAction({ type: 'hideTip' });
+    };
+    const onPointDragging = (dataIndex: number, pos: number[]) => {
+      const dataCopy = [...dataRef.current];
+      dataCopy[dataIndex] = myChart.convertFromPixel('grid', pos);
+      setData(dataCopy);
+      updatePosition();
+    };
+    const updatePosition = () => {
+      setGraphic(
+        dataRef.current.map<GraphicComponentLooseOption>(function (item, dataIndex) {
+          return {
+            type: 'circle',
+            position: myChart.convertToPixel('grid', item),
+            shape: { cx: 0, cy: 0, r: symbolSize / 2 },
+            invisible: true,
+            draggable: true,
+            ondrag(_dx: number, _dy: number) {
+              // @ts-expect-error
+              onPointDragging(dataIndex, [this.x, this.y]);
+            },
+            onmousemove: () => showTip(dataIndex),
+            onmouseout: () => hideTip(),
+            z: 100,
+          } as GraphicComponentLooseOption;
+        })
+      );
+    };
+    window.addEventListener('resize', updatePosition);
+    myChart.on('dataZoom', updatePosition);
+    updatePosition();
+    return () => {
+      window.removeEventListener('reset', updatePosition);
+      myChart.off('dataZoom', updatePosition);
+    };
+  }, []);
+
+  return (
+    <LineChart
+      ref={chartRef}
+      style={{ width: 480, height: 360 }}
+      xAxis={{ min: -100, max: 70, type: 'value', axisLine: { onZero: false } }}
+      yAxis={{ min: -30, max: 60, type: 'value', axisLine: { onZero: false } }}
+      grid={{ top: '8%', bottom: '12%' }}
+      series={[{ id: 'a', type: 'line', smooth: true, symbolSize, data }]}
+    >
+      <Title title={{ text: 'Try Dragging these Points', left: 'center' }} />
+      <Tooltip
+        tooltip={{
+          triggerOn: 'none',
+          formatter(params) {
+            if (Array.isArray(params)) return '';
+            const data = params.data as [number, number];
+            return 'X: ' + data[0].toFixed(2) + '<br>Y: ' + data[1].toFixed(2);
+          },
+        }}
+      />
+      <DataZoom
+        dataZoom={[
+          { type: 'slider', xAxisIndex: 0, filterMode: 'none' },
+          { type: 'slider', yAxisIndex: 0, filterMode: 'none' },
+          { type: 'inside', xAxisIndex: 0, filterMode: 'none' },
+          { type: 'inside', yAxisIndex: 0, filterMode: 'none' },
+        ]}
+      />
+      <Graphic graphic={graphic} />
+    </LineChart>
+  );
+}
