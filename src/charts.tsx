e69de6ff3c7ba0e21e930c54f6d9a724406f64b2
Tmk <i@tmk.im>
2025-08-18 00:36:39 +0800

    feat: support composing charts

--- src/charts.tsx
+++ src/charts.tsx
@@ -7,7 +7,16 @@ import {
 } from 'echarts/charts';
 import type { ComposeOption } from 'echarts/core';
 import { GridComponent, type GridComponentOption } from 'echarts/components';
-import { ChartContext, defaultSetOptionOpt, echarts, useInitialChartContext, useRegister } from './shared.js';
+import type { ComponentOption } from 'echarts/types/src/util/types.js';
+import {
+  ChartContext,
+  defaultSetOptionOpt,
+  echarts,
+  useInitialChartContext,
+  useRegister,
+  type EChartExt,
+  type Simplify,
+} from './shared.js';
 
 function assignForwardedRef<T>(ref: React.ForwardedRef<T>, value: T | null) {
   if (!ref) return;
@@ -15,52 +24,76 @@ function assignForwardedRef<T>(ref: React.ForwardedRef<T>, value: T | null) {
   else ref.current = value;
 }
 
-type LineChartOption = echarts.EChartsCoreOption &
-  ComposeOption<LineSeriesOption | GridComponentOption | PieSeriesOption>;
-
-interface LineChartProps extends LineChartOption {
+interface ChartBaseProps<T extends readonly ChartComponentType<any>[]> {
   className?: string;
   style?: React.CSSProperties;
   containerProps?: React.HTMLAttributes<HTMLDivElement>;
+  compose?: [...T];
+  children?: React.ReactNode;
 }
 
-export const LineChart = forwardRef(
-  (
-    { className, style, containerProps, children, ...props }: React.PropsWithChildren<LineChartProps>,
-    ref: React.ForwardedRef<echarts.ECharts>
-  ) => {
-    const containerRef = useRef<HTMLDivElement>(null);
-    const chartRef = useRef<echarts.ECharts | null>(null);
-    const ctx = useInitialChartContext();
+type InferChartComponentOption<
+  T extends readonly ChartComponentType<any>[],
+  U = T[number]
+> = U extends ChartComponentType<infer P> ? P : never;
 
-    useRegister((echarts) => {
-      // TODO: Make different charts composable.
-      echarts.use([EChartLineChart, GridComponent, EChartPieChart]);
-    });
+interface ChartComponentType<T extends ComponentOption> {
+  <U extends readonly ChartComponentType<any>[] = []>(
+    props: ChartBaseProps<U> &
+      echarts.EChartsCoreOption &
+      Simplify<ComposeOption<T | InferChartComponentOption<U>>> & { ref?: React.Ref<echarts.ECharts> }
+  ): React.JSX.Element;
 
-    useLayoutEffect(() => {
-      const chart = (chartRef.current = echarts.init(containerRef.current, null));
-      assignForwardedRef(ref, chart);
-      return () => {
-        chart.dispose();
-        assignForwardedRef(ref, null);
-      };
-    }, []);
+  ext: EChartExt;
+}
 
-    useLayoutEffect(() => {
-      const chart = chartRef.current;
-      if (!chart) return;
-      for (const opt of ctx.options) chart.setOption(opt, defaultSetOptionOpt);
-      chart.setOption(props, defaultSetOptionOpt);
-      chart.setOption(chart.getOption(), { notMerge: true }); // used for toolbox.restore snapshot
-      ctx.options.length = 0;
-    });
+function defineChart<T extends ComponentOption>(ext: EChartExt) {
+  const ChartComponent = forwardRef(
+    (
+      { className, style, containerProps, compose, children, ...props }: ChartBaseProps<any>,
+      ref: React.ForwardedRef<echarts.ECharts>
+    ) => {
+      const containerRef = useRef<HTMLDivElement>(null);
+      const chartRef = useRef<echarts.ECharts | null>(null);
+      const ctx = useInitialChartContext();
 
-    return (
-      <ChartContext.Provider value={ctx}>
-        <div className={className} style={style} {...containerProps} ref={containerRef} />
-        {children}
-      </ChartContext.Provider>
-    );
-  }
-) as (props: React.PropsWithChildren<LineChartProps> & { ref?: React.Ref<echarts.ECharts> }) => React.JSX.Element;
+      useRegister((echarts) => {
+        echarts.use([ext, compose?.map((comp) => comp.ext).flat() || []].flat());
+      });
+
+      useLayoutEffect(() => {
+        const chart = (chartRef.current = echarts.init(containerRef.current, null));
+        assignForwardedRef(ref, chart);
+        return () => {
+          chart.dispose();
+          assignForwardedRef(ref, null);
+        };
+      }, []);
+
+      useLayoutEffect(() => {
+        const chart = chartRef.current;
+        if (!chart) return;
+        for (const opt of ctx.options) chart.setOption(opt, defaultSetOptionOpt);
+        chart.setOption(props, defaultSetOptionOpt);
+        chart.setOption(chart.getOption(), { notMerge: true }); // used for toolbox.restore snapshot
+        ctx.options.length = 0;
+      });
+
+      return (
+        <ChartContext.Provider value={ctx}>
+          <div className={className} style={style} {...containerProps} ref={containerRef} />
+          {children}
+        </ChartContext.Provider>
+      );
+    }
+  ) as unknown as ChartComponentType<T>;
+  ChartComponent.ext = ext;
+  return ChartComponent;
+}
+
+export const LineChart = /*#__PURE__*/ defineChart<LineSeriesOption | GridComponentOption>([
+  EChartLineChart,
+  GridComponent,
+]);
+
+export const PieChart = /*#__PURE__*/ defineChart<PieSeriesOption>([EChartPieChart]);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { LineChart } from './charts.js';
+export { LineChart, PieChart } from './charts.js';
 export {
   Dataset,
   DataZoom,
